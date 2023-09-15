c2ddbebda08780e80b52d9737a647ccde75ae26b
Ems4 <103748111+Ems4@users.noreply.github.com>
2023-09-15 19:03:13 +0200

    Add files via upload

---
+++ Exercice 1,2 - Modeling.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 15 14:37:57 2023
+
+@author: deneuvilleemma
+"""
+
+def main():
+    V_membrane = float(-75)
+    spike = input("input spike value in mV")
+    InputSpikeVal = int(spike)
+    V_membrane = V_membrane = InputSpikeVal
+    print(V_membrane)
+    
+    
+V_membrane = float(-75)
+spike = input(50)
+InputSpikeVal = int(spike)
+V_membrane = V_membrane + InputSpikeVal 
+print(V_membrane)
+
+
+#Exercice 2 ? didn't really understood tho
+
+# Initialize the variable with value .1
+variable_step1 = 0.1
+
+# Compute the value of the variable by multiplying it by 0.7
+variable_step2 = variable_step1 * 0.7
+
+# Initialize the variable with value 1.0
+variable_step3 = 1.0
+
